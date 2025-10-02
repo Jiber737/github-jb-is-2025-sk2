565760b8dd9cf2db52a1ef2c463ddabd5b195553
Jiří Bertl <jiri.bertl@tul.cz>
2025-10-02 14:05:26 +0200

    #3 vzpis rady- vstup prvni hodnoty

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,26 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************************");
+    Console.WriteLine("****************Výpis řady*****************");
+    Console.WriteLine("*******************************************");
+    Console.WriteLine("****************2.10.2025******************");
+    Console.WriteLine("*******************************************");
+    Console.WriteLine();
+
+    //vstup do programu tadz muyu psat literallz cokoliv a nema to nejmensi efekt na tenhle prográmek
+    //Console.WriteLine("yadej prvni cislo rady");
+    //int first = int.Parse(Console.ReadLine());
+
+
+    //Console.WriteLine("pro opakování programu stiskni klávesu a");
+    //again = Console.ReadLine();
+    Console.WriteLine("yadej prvni cislo");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.WriteLine("to se ti moc nepodarilo, ykus to zadat znovu");
+    }
+
+}
