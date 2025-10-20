976b5bc2db123f7924df24b68b5ec6b0375e3ba2
Jiří Bertl <jiri.bertl@tul.cz>
2025-10-20 16:55:12 +0200

    #6 mnohem rychlejší cesta součtu cifer

---
+++ IS-Programy/program002b-soucet-cifer/Program.cs
@@ -0,0 +1,47 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************** SOucet cislic ***************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jiří Bertl *******************");
+    Console.WriteLine("************** 20.10.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu, ale špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    // Vstup hodnoty do programu, řešený lépe
+    Console.Write("Zadejte svoje číslo, kde chcete provést ciferný součet (celé číslo): ");
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
+    }
+
+    //first = int.Parse(Console.ReadLine());
+    string cislicko =  first.ToString();
+    int sum = 0;
+    foreach (char c in cislicko)
+    {
+        sum += int.Parse(c.ToString());
+    }
+    Console.WriteLine("ciferný součet čísla {0} je {1}", first, sum);   
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
+
+
