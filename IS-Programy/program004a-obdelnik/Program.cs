9beeb921ce2707088e27b3d4297baa56421dfaf8
Jiří Bertl <jiri.bertl@tul.cz>
2025-10-25 16:58:30 +0200

    #8 Obdelnik

---
+++ IS-Programy/program004a-obdelnik/Program.cs
@@ -0,0 +1,66 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************ Tvorba obdelniku **************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jiří Bertl *******************");
+    Console.WriteLine("************** 23.10.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu, ale špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    // Vstup hodnoty do programu, řešený lépe
+    Console.Write("Zadejte šířku (celé číslo): ");
+    int width;
+    while (!int.TryParse(Console.ReadLine(), out width))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo  znovu: ");
+    }
+
+    Console.Write("Zadejte výšku (celé číslo): ");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte první druhé  znovu: ");
+    }
+
+    // Vypisování obdelníku
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            Console.Write("*");
+            if (i % 2 == 1)
+            {
+                Console.Write("*");
+            }
+else if 
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+    
+
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
+
+
