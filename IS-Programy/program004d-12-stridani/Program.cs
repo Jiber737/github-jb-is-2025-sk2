00c860399d3d4785b5b830f93033fcd491569f00
Jiří Bertl <jiri.bertl@tul.cz>
2025-10-28 15:12:56 +0100

    #8 jednoduchy obrazec - stridani hvezdicek

---
+++ IS-Programy/program004d-12-stridani/Program.cs
@@ -0,0 +1,51 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*************** Obrázky ********************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************** Jiří Bertl ******************");
+    Console.WriteLine("************** 2.10.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu, ale špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    // Vstup hodnoty do programu, řešený lépe
+    Console.Write("Zadejte velikost obrazce (celé číslo): ");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            if ((i + j) % 2 == 0)
+                Console.Write("  ");
+            else
+                Console.Write("* ");
+                System.Threading.Thread.Sleep(5);
+        }
+        Console.WriteLine();
+    }
+
+
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
+
+
