763ea20845b9aafc8618332d85d247f4c41dc156
Jiří Bertl <jiri.bertl@tul.cz>
2025-11-03 15:23:42 +0100

    #8 mrizka

---
+++ IS-Programy/program004e-8-mrizka/Program.cs
@@ -0,0 +1,46 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("***************** Mřížka *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jiří Bertl *******************");
+    Console.WriteLine("************** 3.11.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu, ale špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    // Vstup hodnoty do programu, řešený lépe
+    Console.Write("Zadejte výšku mřížky (celé číslo): ");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
+    }
+   
+   for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= height; j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+                Console.Write("  ");
+            else
+                Console.Write("* ");
+        }
+        Console.WriteLine();
+    }
+
+    
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
+
+
