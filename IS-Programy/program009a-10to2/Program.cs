d71c797a60b04d9cca3098e9e1165886a07775d5
Jiří Bertl <jiri.bertl@tul.cz>
2025-11-27 13:11:24 +0100

    #13 Binarka done in 34 minutes

---
+++ IS-Programy/program009a-10to2/Program.cs
@@ -0,0 +1,64 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("*********** Výpis číselné řady *************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************** Jiří Bertl ******************");
+    Console.WriteLine("************** 2.10.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    // Vstup hodnoty do programu, ale špatně řešený
+    //Console.Write("Zadejte první číslo řady: ");
+    //int first = int.Parse(Console.ReadLine());
+
+    // Vstup hodnoty do programu, řešený lépe
+    Console.Write("Zadejte číslo, jež nutně potřebujete převést do binární soustavy (celé číslo): ");
+    int cislo;
+    while (!int.TryParse(Console.ReadLine(), out cislo))
+    {   
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("Nezadali jste celé číslo. Zadejte prosíííím číslo znovu: ");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    int zbytek = cislo;
+    string binarni = "";
+    Console.WriteLine();
+    
+    do {
+        if (zbytek % 2 == 0)
+        {
+            binarni += "0";
+        }
+        else
+        {
+            binarni += "1";
+        }
+        zbytek /= 2;
+        cislo /= 2;
+    } while (cislo != 0);
+
+    Console.WriteLine("tvoje binární číslo je: ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    for (int i = binarni.Length - 1; i >= 0; i--)
+    {
+        Console.Write(binarni[i]);
+    }
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine();
+
+
+
+  
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
+
+
